d96992b20d454a6a22f4bc54ba9cc6e68a492971
Miloš Dojčinović <58708252+Doyche@users.noreply.github.com>
2020-05-27 15:33:52 +0200

    Add files via upload

---
+++ config/google-calendar.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+     * Path to the json file containing the credentials.
+     */
+    'service_account_credentials_json' => storage_path('app/google-calendar/laravel-calendar-278413-d2062bcfd31f.json'),
+
+    /*
+     *  The id of the Google Calendar that will be used by default.
+     */
+    'calendar_id' => env('GOOGLE_CALENDAR_ID'),
+
+    /*
+ *  The user e-mail of the Google Calendar that will be impersonated.
+ */
+    'user_to_impersonate' => env('USER_TO_IMPERSONATE', ''),
+];
