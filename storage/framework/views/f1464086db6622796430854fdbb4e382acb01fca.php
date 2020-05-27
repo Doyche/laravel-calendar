0f58eb49f8b87741e8d8ff972064573f1f65427b
Miloš Dojčinović <58708252+Doyche@users.noreply.github.com>
2020-05-27 15:40:25 +0200

    Add files via upload

---
+++ storage/framework/views/f1464086db6622796430854fdbb4e382acb01fca.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+
+    <!-- Scripts -->
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <app-container></app-container>
+    </div>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\laravel_7\g-calendar\resources\views/spa.blade.php ENDPATH**/ ?>
